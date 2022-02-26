c60afc8c79b40ae4d5e6d25063e740db48a71ab2
Edson J Vargas <edson.jhair@yahoo.com>
2022-02-25 22:26:20 -0500

    ADD: animaciones

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import { CategoListry } from './components/Category'
-import { GlobalStyle } from './components/GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { Logo } from './components/Logo'
