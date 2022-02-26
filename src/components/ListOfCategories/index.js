0e1321cb3940b1837f33e669900caaf3193951ca
Edson J Vargas <edson.jhair@yahoo.com>
2022-02-25 23:18:09 -0500

    ADD: hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import db from '../../../api/db.json'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    window.fetch('https://petgram-server-gtrnb8zqu-edsonv.vercel.app/categories')
+      .then(response => response.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
   return (
     <List>
       {
-        db.categories.map(category => (
+        categories.map(category => (
           <Item key={category.id}>
             <Category {...category} />
           </Item>
